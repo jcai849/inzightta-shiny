a3544dee10acdfcbff82ec76de8ff3e38388a88f
bernice99 <bcu120@aucklanduni.ac.nz>
2020-01-22 20:28:23 +1300

    server code used to create the word tree plot from googleVis

---
+++ R/word_tree_server.R
@@ -0,0 +1,11 @@
+output$shinytest <- renderGvis({
+  input$create_tree
+  arts <- merged() %>% unnest_tokens(text, text, token = "sentences")
+  isolate(gvisWordTree(arts, textvar = "text",
+                       options = list(fontName = "Times-Roman",
+                                      wordtree = sprintf("{word: '%s', type: '%s'}",
+                                                         input$firstword, input$tree_type),
+                                      height = "1000px", width = "800px"
+                       )
+  ))
+})
